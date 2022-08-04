dc806db7526b1103f5d115e7caa902265c23be45
quochuy2405 <huy01682312920@gmail.com>
2022-08-05 01:32:45 +0700

    update

--- public/locales/vi/common.ts
+++ public/locales/vi/common.ts
@@ -1,67 +1,68 @@
 const SET = {
   header: {
-    about: "Thông tin",
+    about: "About",
     home: "Home",
     blog: "Blog"
   },
-  navlinks: {
+  navlinks:{
     ourservice: "Our Service",
     aboutus: "About Us",
     foodshop: "Food Shop",
     petshop: "Pet Shop",
     contact: "Contact",
     review: "Review",
+    
   },
-  btn: {
-    login: "Đăng nhập",
-    buy: "Đăng ký",
+   btn:{
+    login: "Sign in",
+    buy: "Buy Now ",
     details: "View Details ",
-    logout: "Đăng xuất ",
-    sendMail: "Gửi yêu cầu",
-    changeLang: "EN"
+    logout: "Sign out ",
+    sendMail: "Send Email",
+    changeLang:"VI"
   },
-  overview: {
-    title: "Các Khóa Học Với Giảng Viên Tâm Huyết ",
+  overview:{
+    title: "Taking care for your smart cats",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero natus aliquam aspernatur nobis ipsam rerum vero soluta numquam iusto porro ea ad molestias optio et, architecto ab. Totam, sint fuga!",
   },
   courses: {
-    count: "Đã đăng ký"
+    count:"Registed"
   },
   home_page: {
-    findTheCourse: "Tìm kiếm khóa học bạn muốn",
-    developQualityTitle: "Phát triển bản thân",
-    developQualityDes: "Yêu thích gì đó ",
+    findTheCourse: "Find The Course",
+    developQualityTitle: "Develop Your Quality",
+    developQualityDes: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime eligendi ex voluptatem aut consectetur hic vitae esse odio tempora",
     titleService:'Service Category'
   },
   register_page: {
-    classCode: "Mã lớp",
-    name: "Họ và tên",
-    phone: "Số điện thoại",
-    birthDay: "Ngày sinh",
+    classCode: "Class Code",
+    name: "Full Name",
+    phone: "Phone Number",
+    birthDay: "Day of Birth",
     email: "Email",
-    agreeTerms: "Đồng ý với tất cả điều khoản ?"
+    agreeTerms:"Agree to all terms ?"
   },
-  course_page: {
-     title: "Khóa học ngay bên dưới. Hãy đăng ký và gửi thông tin về cho Ms.Quynh nhé. ",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero natus aliquam aspernatur nobis ipsam rerum vero soluta numquam iusto porro ea ad molestias optio et, architecto ab. Totam, sint fuga!",
-     findTheCourse: "Tất cả khóa học",
+   course_page: {
+    title: "Khóa học ngay bên dưới. Hãy đăng ký và gửi thông tin về cho Ms.Quynh nhé. ",
+     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero natus aliquam aspernatur nobis ipsam rerum vero soluta numquam iusto porro ea ad molestias optio et, architecto ab. Totam, sint fuga!",
+    findTheCourse: "All Courses",
     developQualityTitle: "Develop Your Quality",
     developQualityDes:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime eligendi ex voluptatem aut consectetur hic vitae esse odio tempora"
   },
   progress_page: {
-    titleList: 'Danh sách xác nhận',
+    titleList: 'Manager Courses ',
     headerTable: {
-      col1: 'Mã lớp',
-      col2: 'Tên khóa học',
-      col3: 'Tên học sinh',
-      col4:'Thời gian khai giảng',
-      col5: 'Trạng thái',
-      col6: 'Hủy bỏ',
+      col1: 'Class ID',
+      col2: 'Course Name',
+      col3: 'Student Name',
+      col4: 'Start date',
+      col5: 'Status',
+      col6: 'Cancel',
     },
     button: {
-      accept: "Đã xác nhận",
-      wait: "Chờ xác nhận",
-      reject:'Hủy'
+      accept: "Accepted",
+      wait: "Awating",
+      reject:'Reject'
     }
    }
 }
